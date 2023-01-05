076828155bd1a0f756e2389757f818e2a497cf43
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-05 19:58:25 -0300

    chore: sql tables

---
+++ tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE clients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  phone VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE cakes (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55) NOT NULL,
+  price INT NOT NULL,
+  phone VARCHAR(11) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  client_id INT REFERENCES "clients"("id") NOT NULL,
+  cake_id INT REFERENCES "cakes"("id") NOT NULL,
+  quantity INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  total_price INT NOT NULL
+);

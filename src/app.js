4357c39846acbce97b96f6adbbafad4601c9ba92
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-05 20:10:39 -0300

    feat: initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running in port " + PORT));

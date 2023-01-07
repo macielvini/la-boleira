8b7a463a6bd7d16406a4438449a98c77fb3f53ce
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-06 21:14:38 -0300

    feat: find all orders route

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -19,3 +19,78 @@ export async function create(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function findAll(req, res) {
+  const date = req.query.date;
+
+  try {
+    let orders;
+
+    if (date) {
+      orders = await connection.query(
+        `
+          SELECT
+            json_build_object(
+              'id', cl.id,
+              'name', cl.name,
+              'address', cl.address,
+              'phone', cl.phone
+            ) AS client,
+            json_build_object(
+              'id', ck.id,
+              'name', ck.name,
+              'price', ck.price,
+              'description', ck.description,
+              'image', ck.image
+            ) AS cake,
+            o.id AS "orderId",
+            o.created_at AS "createdAt",
+            o.quantity,
+            o.total_price AS "totalPrice"
+          FROM orders o
+          JOIN clients cl
+          ON o.client_id = cl.id
+          JOIN cakes ck
+          ON o.cake_id = ck.id 
+          WHERE date_trunc('day', created_at) = $1;
+    `,
+        [date]
+      );
+    } else {
+      orders = await connection.query(
+        `
+          SELECT
+            json_build_object(
+              'id', cl.id,
+              'name', cl.name,
+              'address', cl.address,
+              'phone', cl.phone
+            ) AS client,
+            json_build_object(
+              'id', ck.id,
+              'name', ck.name,
+              'price', ck.price,
+              'description', ck.description,
+              'image', ck.image
+            ) AS cake,
+            o.id AS "orderId",
+            o.created_at AS "createdAt",
+            o.quantity,
+            o.total_price AS "totalPrice"
+          FROM orders o
+          JOIN clients cl
+          ON o.client_id = cl.id
+          JOIN cakes ck
+          ON o.cake_id = ck.id 
+        `
+      );
+    }
+
+    if (!orders.rowCount) return res.status(404).send([]);
+
+    res.send(orders.rows.reverse());
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create } from "../controllers/order.controller.js";
+import { create, findAll } from "../controllers/order.controller.js";
 import { validate } from "../middlewares/validateOrder.middleware.js";
 import { validateSchema400 } from "../middlewares/validateSchema.middleware.js";
 import { orderSchema } from "../models/order.model.js";
@@ -7,5 +7,6 @@ import { orderSchema } from "../models/order.model.js";
 const router = Router();
 
 router.post("/order", validateSchema400(orderSchema), validate, create);
+router.get("/order", findAll);
 
 export default router;

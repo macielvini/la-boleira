bde0ff6d6b28b8dce985f88bd26e94bc1f4903b6
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-08 11:36:40 -0300

    feat: get client order by id

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -18,3 +18,43 @@ export async function create(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function findAll(req, res) {
+  const { id } = req.params;
+
+  try {
+    const findClientId = await connection.query(
+      `
+      SELECT * FROM clients WHERE id = $1;
+    `,
+      [id]
+    );
+
+    if (!findClientId.rowCount) {
+      return res.sendStatus(404);
+    }
+
+    const orders = await connection.query(
+      `
+      SELECT 
+      o.id AS "orderId",
+      o.quantity,
+      o.created_at AS "createdAt",
+      o.total_price AS "totalPrice",
+      ck.name AS "cakeName"
+      FROM clients c
+      JOIN orders o
+      ON o.client_id = c.id
+      JOIN cakes ck
+      ON o.cake_id = ck.id
+      WHERE o.client_id = $1;
+    `,
+      [id]
+    );
+
+    res.send(orders.rows);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
